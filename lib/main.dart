53fba26345d3484f0e3a14aec7b71776fdd2beba
saifudinzezy <saifudin.zezy7@gmail.com>
2022-06-04 13:56:07 +0700

    scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -10,46 +10,15 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello world"),
-        ),
-        body: Center(
-          child: BiggerText(
-            text: "Hello World",
-          ),
-        ),
-      ),
+      home: FirstScreen(),
     );
   }
 }
 
-class BiggerText extends StatefulWidget {
-  final String texst;
-
-  const BiggerText({required this.texst});
-
-  @override
-  _BiggerTextState createState() => _BiggerTextState();
-}
-
-class _BiggerTextState extends State<BiggerText> {
-  double _textSize = 16.0;
+class FirstScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(widget.texst, style: TextStyle(fontSize: _textSize)),
-        ElevatedButton(
-          child: Text("Perbesar"),
-          onPressed: (){
-            setState(() {
-              _textSize = 32.0;
-            });
-          })
-      ],
-    );
+    return Scaffold();
   }
 }
