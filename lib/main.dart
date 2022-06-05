af2e84afa021434ffb415a6a585ff2b7c6044813
saifudinzezy <saifudin.zezy7@gmail.com>
2022-06-05 21:01:52 +0700

    widget image

--- lib/main.dart
+++ lib/main.dart
@@ -3,55 +3,28 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-
-  @override
-  State createState() {
-    return _FirstScreenState();
-  }
-}
-class _FirstScreenState extends State<MyApp> {
-  TextEditingController _controller = TextEditingController();
+class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('First Screen'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                hintText: 'Write your name here...',
-                labelText: 'Your Name',
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('First Screen'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Image.network(
+                'https://picsum.photos/200/300',
+                width: 200,
+                height: 200,
               ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        content: Text('Hello, ${_controller.text}'),
-                      );
-                    });
-              },
-              child: Text('Submit'),
-            )
-          ],
+              Image.asset('images/chicken.png', width: 200, height: 200),
+            ],
+          )
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 }
