c9e9123c1cbe3fb7bdb7c9b59df988b4555271b7
saifudinzezy <saifudin.zezy7@gmail.com>
2022-06-05 19:04:48 +0700

    elevated button

--- lib/main.dart
+++ lib/main.dart
@@ -11,61 +11,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DetailScreen(),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 26.0),
-            child: Text(
-              "Farm House Lembang",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  children: [
-                    Icon(Icons.calendar_today),
-                    SizedBox(height: 8.0),
-                    Text('Open Everyday')
-                  ],
-                ),
-                Column(
-                  children: [
-                    Icon(Icons.access_time),
-                    SizedBox(height: 8.0),
-                    Text("09:00 - 20:00")
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Icon(Icons.monetization_on),
-                    SizedBox(height: 8.0),
-                    Text('RP 25.000'),
-                  ],
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Widget BUtton'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              child: ElevatedButton(onPressed: (){}, child: Text("Tombol")),
+            )
+          ],
+        ),
+      )
     );
   }
 }
