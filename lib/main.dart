4892c929677e765f65d33d3bd7b570082de13692
saifudinzezy <saifudin.zezy7@gmail.com>
2022-06-05 20:45:29 +0700

    input text field

--- lib/main.dart
+++ lib/main.dart
@@ -3,40 +3,55 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  String? language;
+class MyApp extends StatefulWidget {
+
+  @override
+  State createState() {
+    return _FirstScreenState();
+  }
+}
+class _FirstScreenState extends State<MyApp> {
+  TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Wisata Bandung',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Widget BUtton'),
-        ),
-        body: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
           children: [
-            Container(
-              child: DropdownButton<String>(
-                  items: const <DropdownMenuItem<String>>[
-                    DropdownMenuItem<String>(child: Text("Dart"), value: "Dart",),
-                    DropdownMenuItem<String>(child: Text("Java"), value: "Java",)
-                  ],
-                  value: language,
-                  hint: Text("Select language"),
-                  onChanged: (String? value) {
-                    setState(() {
-                      language = value;
-                    });
-                  },
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                hintText: 'Write your name here...',
+                labelText: 'Your Name',
               ),
             ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        content: Text('Hello, ${_controller.text}'),
+                      );
+                    });
+              },
+              child: Text('Submit'),
+            )
           ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
