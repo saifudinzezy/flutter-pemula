adc9e315cd40225b7736616fce8191cdfdbf3dc6
saifudinzezy <saifudin.zezy7@gmail.com>
2022-06-11 20:35:55 +0700

    flexible

--- lib/main.dart
+++ lib/main.dart
@@ -13,46 +13,94 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Column(
-          children: [
-            Expanded(
-                child: Container(
-                  color: Colors.red,
-                )
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.orange,
+        home: ExpandedFlexiblePage()
+    );
+  }
+}
+
+class ExpandedFlexiblePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  ExpandedWidget(),
+                  FlexibleWidget(),
+                ],
               ),
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.yellow,
+              Row(
+                children: [
+                  ExpandedWidget(),
+                  ExpandedWidget(),
+                ],
               ),
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.green,
+              Row(
+                children: [
+                  FlexibleWidget(),
+                  FlexibleWidget(),
+                ],
               ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                color: Colors.blue,
+              Row(
+                children: [
+                  FlexibleWidget(),
+                  ExpandedWidget(),
+                ],
               ),
+            ],
+          )
+      ),
+    );
+  }
+}
+
+class ExpandedWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.teal,
+          border: Border.all(color: Colors.white),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            'Expanded',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
             ),
-            Expanded(
-              child: Container(
-                color: Colors.indigo,
-              ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FlexibleWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.tealAccent,
+          border: Border.all(color: Colors.white),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            'Flexible',
+            style: TextStyle(
+              color: Colors.teal,
+              fontSize: 24,
             ),
-            Expanded(
-              child: Container(
-                color: Colors.purple,
-              ),
-            ),
-          ],
-        )
+          ),
+        ),
+      ),
     );
   }
 }
