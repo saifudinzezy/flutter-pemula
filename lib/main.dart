5613442de02b1bc02daff94e6b14cb0056d70d7e
saifudinzezy <saifudin.zezy7@gmail.com>
2022-06-11 18:23:38 +0700

    add dinamyc

--- lib/main.dart
+++ lib/main.dart
@@ -9,71 +9,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wisata Bandung',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Widget ListView'),
+        title: 'Wisata Bandung',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-        body: ListView(
-          children: [
-            Container(
-              height: 250,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                border: Border.all(color: Colors.black),
-              ),
-              child: Center(
-                child: Text(
-                  '1',
-                  style: TextStyle(fontSize: 50),
-                ),
-              ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Widget ListView'),
             ),
-            Container(
-              height: 250,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                border: Border.all(color: Colors.black),
-              ),
-              child: Center(
-                child: Text(
-                  '2',
-                  style: TextStyle(fontSize: 50),
-                ),
-              ),
-            ),
-            Container(
-              height: 250,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                border: Border.all(color: Colors.black),
-              ),
-              child: Center(
-                child: Text(
-                  '3',
-                  style: TextStyle(fontSize: 50),
-                ),
-              ),
-            ),
-            Container(
-              height: 250,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                border: Border.all(color: Colors.black),
-              ),
-              child: Center(
-                child: Text(
-                  '4',
-                  style: TextStyle(fontSize: 50),
-                ),
-              ),
-            ),
-          ],
+            body: ListView(
+              children: numberList.map((number){
+                return Container(
+                  height: 250,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '1',
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ),
+                );
+              }).toList(),
+            )
         )
-      )
     );
   }
 }
