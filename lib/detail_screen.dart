6805d6eeb44da3e0b1abd4da1b2c92e35cfedf16
saifudinzezy <saifudin.zezy7@gmail.com>
2022-06-12 18:29:54 +0700

    add screen

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,25 +1,54 @@
-import 'package:flutter/cupertino.dart';
+import 'package:dicoding/model/tourism_place.dart';
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatelessWidget {
   var informationTextStyle = TextStyle(fontFamily: 'Oxygen');
+  final TourismPlace place;
+
+  DetailScreen({required this.place});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("wisata bandung")),
-      body:
-      SingleChildScrollView(
+      // appBar: AppBar(title: Text("wisata bandung")),
+      body: SingleChildScrollView(
       // SafeArea(
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Image.asset('images/farm-house.jpg'),
+              Stack(
+                children: <Widget>[
+                  Image.asset(place.imageAsset),
+                  SafeArea(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.grey,
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.arrow_back,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ),
+                          FavoriteButton(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
               Container(
                 margin: EdgeInsets.only(top: 26.0),
                 child: Text(
-                  "Farm House Lembang",
+                  place.name,
                   textAlign: TextAlign.center,
                   style: TextStyle(
                       fontSize: 30.0,
@@ -37,53 +66,50 @@ class DetailScreen extends StatelessWidget {
                       children: [
                         Icon(Icons.calendar_today),
                         SizedBox(height: 8.0),
-                        Text('Open Everyday', style: informationTextStyle,)
+                        Text( place.openDays, style: informationTextStyle,)
                       ],
                     ),
                     Column(
                       children: [
                         Icon(Icons.access_time),
                         SizedBox(height: 8.0),
-                        Text("09:00 - 20:00")
+                        Text( place.openTime, style: informationTextStyle,)
                       ],
                     ),
                     Column(
                       children: <Widget>[
                         Icon(Icons.monetization_on),
                         SizedBox(height: 8.0),
-                        Text('RP 25.000'),
+                        Text( place.ticketPrice, style: informationTextStyle,),
                       ],
                     ),
                   ],
                 ),
               ),
               Container(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  place.description,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontFamily: 'Oxygen',
+                  ),
+                ),
+              ),
+              Container(
                 height: 150,
                 child: ListView(
                   scrollDirection: Axis.horizontal,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        child: Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
+                  children: place.imageUrls.map((url) {
+                      return Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: ClipRRect(
                           borderRadius: BorderRadius.all(Radius.circular(10)),
-                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg')
-                      ),
-                    ),
-                  ],
+                          child: Image.network(url),
+                        ),
+                      );
+                  }).toList(),
                 ),
               ),
             ],
@@ -91,6 +117,32 @@ class DetailScreen extends StatelessWidget {
         ),
       // )
       )
+    );
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  @override
+  _FavoriteButtonState createState() => _FavoriteButtonState();
+}
+
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isFavorite ? Icons.favorite : Icons.favorite_border,
+        color: Colors.red,
+      ),
+      onPressed: () {
+        setState(() {
+          isFavorite = !isFavorite;
+        });
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:dicoding/main_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:dicoding/detail_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DetailScreen(),
+      home: MainScreen(),
     );
   }
 }
